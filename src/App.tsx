a10be014ae0a7c112788dedaf5bcc37e2a0983cd
Renan Santos <renan.santos@megaleios.com>
2020-07-09 12:12:19 -0300

    Eslint, Prettier and EditorConfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import {View} from 'react-native';
+import { View } from 'react-native';
 
-// import { Container } from './styles';
-
-const App: React.FC = () => {
-  return <View />;
-};
+const App: React.FC = () => <View />;
 
 export default App;
