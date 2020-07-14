2faff5b943396805a3630a0ea41bf32c57cd6794
RenanKanu <renankanu@gmail.com>
2020-07-13 23:29:43 -0300

    Create interface Job from request API

--- src/screens/Job/index.tsx
+++ src/screens/Job/index.tsx
@@ -1,9 +1,24 @@
+/* eslint-disable camelcase */
 import React, { useEffect, useState } from 'react';
 import { Container, ContainerSearch } from './styles';
 import api from '../../services/api';
 
+interface Job {
+  id: string;
+  url: string;
+  title: string;
+  company_name: string;
+  category: string;
+  tags: Array<string>;
+  job_type: string;
+  publication_date: string;
+  candidate_required_location: string;
+  salary: string;
+  description: string;
+}
+
 const Job: React.FC = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<Job[]>([]);
   useEffect(() => {
     const requestRemoteJobs = async () => {
       try {
@@ -15,6 +30,10 @@ const Job: React.FC = () => {
     };
     requestRemoteJobs();
   }, []);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
   return (
     <Container>
