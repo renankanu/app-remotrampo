daed5a72da81e9017f451148697170fdf94138b1
RenanKanu <renankanu@gmail.com>
2020-07-15 12:43:47 -0300

    styled HTML from API

--- src/screens/JobDetails/index.tsx
+++ src/screens/JobDetails/index.tsx
@@ -1,9 +1,19 @@
 /* eslint-disable camelcase */
 import React from 'react';
-import { Image } from 'react-native';
 import { useRoute } from '@react-navigation/native';
-import { Container } from './styles';
+import HTML from 'react-native-render-html';
+import { IGNORED_TAGS } from 'react-native-render-html/src/HTMLUtils';
+import { Dimensions } from 'react-native';
+import {
+  Container,
+  CompanyImage,
+  ContainerCenter,
+  Label,
+  Info,
+  ScrollView,
+} from './styles';
 import notFound from '../../assets/images/notFound.png';
+import { Spacer } from '../../styles/index';
 
 interface Job {
   id: string;
@@ -24,21 +34,82 @@ interface RouteParams {
   item: Job;
 }
 
+const tagsStyles = {
+  div: {
+    color: '#FFF',
+    margin: 10,
+  },
+  span: {
+    color: '#FFF',
+  },
+  p: {
+    color: '#FFF',
+    margin: 10,
+  },
+  h1: {
+    color: '#FFF',
+    margin: 10,
+  },
+  h2: {
+    color: '#FFF',
+    margin: 10,
+  },
+  li: {
+    color: '#FFF',
+  },
+  ol: {
+    color: '#FFF',
+  },
+  ul: {
+    color: '#FFF',
+  },
+};
+
 const JobDetails: React.FC = () => {
   const route = useRoute();
   const job = route.params as RouteParams;
 
+  console.log('-----', job.item.description);
+
   return (
-    <Container>
-      <Image
-        style={{ width: 200, height: 200 }}
-        source={
-          job.item.company_logo_url
-            ? { uri: job.item.company_logo_url }
-            : notFound
-        }
-      />
-    </Container>
+    <ScrollView>
+      <Container>
+        <ContainerCenter>
+          <CompanyImage
+            resizeMode="contain"
+            source={
+              job.item.company_logo_url
+                ? { uri: job.item.company_logo_url }
+                : notFound
+            }
+          />
+        </ContainerCenter>
+        <Label>Title</Label>
+        <Info>{job.item.title}</Info>
+        <Spacer height={24} />
+        <Label>Company Name</Label>
+        <Info>{job.item.company_name}</Info>
+        <Spacer height={24} />
+        <Label>category</Label>
+        <Info>{job.item.category}</Info>
+        <Spacer height={24} />
+        <Label>Description</Label>
+        <HTML
+          html={job.item.description}
+          tagsStyles={tagsStyles}
+          ignoredStyles={[
+            'font-family',
+            'line-height',
+            'color',
+            'margin-top',
+            'margin-bottom',
+          ]}
+          baseFontStyle={{ fontFamily: 'JosefinSans-Light' }}
+          imagesMaxWidth={Dimensions.get('window').width}
+        />
+        <Spacer height={24} />
+      </Container>
+    </ScrollView>
   );
 };
 
--- src/screens/JobDetails/styles.ts
+++ src/screens/JobDetails/styles.ts
@@ -1,6 +1,35 @@
 import styled from 'styled-components/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Platform } from 'react-native';
+import customColors from '../../styles/customColors';
+
+export const ScrollView = styled.ScrollView`
+  flex: 1;
+`;
 
 export const Container = styled(SafeAreaView)`
   flex: 1;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+
+export const ContainerCenter = styled.View`
+  align-items: center;
+`;
+
+export const CompanyImage = styled.Image`
+  width: 100px;
+  height: 100px;
+  border-radius: 260px;
+`;
+
+export const Label = styled.Text`
+  color: ${customColors.mischka};
+  font-family: 'JosefinSans-Light';
+  margin-bottom: ${Platform.OS === 'android' ? '0px' : '8px'};
+`;
+export const Info = styled.Text`
+  font-size: 16px;
+  color: ${customColors.mischka};
+  font-family: 'JosefinSans-Medium';
 `;
